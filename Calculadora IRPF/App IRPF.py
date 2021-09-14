6d2e3a250895956405610bc4b674c89317da00a8
Abel <abelrapha@gmail.com>
2021-09-14 11:30:24 -0300

    Update App IRPF.py

--- Calculadora IRPF/App IRPF.py
+++ Calculadora IRPF/App IRPF.py
@@ -1,6 +1,14 @@
 from tkinter import *
-import tkinter
+from tkinter import ttk
+
+colors = {"black": "#050505", "white": "#ffffff", "Blue": "#39579e" , "Gray": "#48494a", "Orange":"#e89b00"}
 
 janela = Tk()
+
+janela.geometry("800x38")
+
+
+
+
 
 janela.mainloop()
